b46b78d3cd8d968f903b4ba71fa794c40eb6bb3b
Daniel <79213142+danmesfin@users.noreply.github.com>
2022-09-12 12:58:46 +0300

    updated countries table

--- containers/Countries/index.tsx
+++ containers/Countries/index.tsx
@@ -1,15 +1,11 @@
-/**
- *
- *
- * @format
- */
 /* eslint-disable @next/next/no-img-element */
 import { Key, useEffect, useState } from "react";
 import Link from "next/link";
+import Spinner from '../../components/Spinner'
 
 export default function Listed() {
   const [data, setData]: any = useState(null);
-  const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
@@ -20,7 +16,37 @@ export default function Listed() {
         setLoading(false);
       });
   }, []);
-
+if (isLoading) {return  (   <div className="overflow-x-auto relative mt-4 mx-2 ">
+<div className="flex flex-col my-2 px-3 py-2 bg-red-100 shadow-sm">
+  <span className=" text-red-600 text-md font-bold">
+    Countries with most active cases
+  </span>
+  <span className="text-gray-400 text-xs font-semibold">
+    click to see detailed information
+  </span>
+</div>
+<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <tr>
+      <th scope="col" className="py-3 px-3">
+        Country
+      </th>
+      <th scope="col" className="py-3 px-3">
+        Cases
+      </th>
+      <th scope="col" className="py-3 px-3">
+        Recovery
+      </th>
+      <th scope="col" className="py-3 px-3">
+        Death
+      </th>
+    </tr>
+  </thead>
+    <tbody>
+      <Spinner />
+      </tbody>
+      </table>
+      </div>);}
   return (
     <div className="overflow-x-auto relative mt-4 mx-2 ">
       <div className="flex flex-col my-2 px-3 py-2 bg-red-100 shadow-sm">
@@ -48,9 +74,6 @@ export default function Listed() {
             </th>
           </tr>
         </thead>
-        {isLoading ? (
-          <></>
-        ) : (
           <tbody>
             {data.slice(0, 14).map((item: Key | null | undefined | any) => (
               <tr
@@ -73,7 +96,6 @@ export default function Listed() {
               </tr>
             ))}
           </tbody>
-        )}
       </table>
     </div>
   );
